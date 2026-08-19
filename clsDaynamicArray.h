e585764399820105d42aed0201f054a586dd6234
Ahmed ALmehdar <ahmed.h.almehdar@gmail.com>
2026-08-20 02:46:04 +0300

    Added To Daynamic Array Class 8 Methods For Delete And Insert Scenarios

--- clsDaynamicArray.h
+++ clsDaynamicArray.h
@@ -87,7 +87,7 @@ public:
 
 	T GetItem(int index)
 	{
-		if (index > _Size || index < 0)
+		if (index >= _Size || index < 0)
 			return -1;
 
 		return OriginalArray[index];
@@ -115,6 +115,111 @@ public:
 		_TempArr = new T[0];
 		delete[] OriginalArray;
 		OriginalArray = _TempArr;
+	}
+
+	bool DeleteItemAt(int index)
+	{
+		if (index >= _Size || index < 0)
+			return false;
+
+		_Size--;
+
+		_TempArr = new T[_Size];
+
+		for (int i = 0; i < index; i++)
+			_TempArr[i] = OriginalArray[i];
+
+
+		for (int i = index + 1; i < _Size + 1; i++)
+			_TempArr[i - 1] = OriginalArray[i];
+
+
+		delete[] OriginalArray;
+		OriginalArray = _TempArr;
+		return true;
+	}
+
+	void DeleteFirstItem()
+	{
+		DeleteItemAt(0);
+	}
+
+	void DeleteLastItem()
+	{
+		DeleteItemAt(_Size - 1);
+	}
+
+	T Find(T Value)
+	{
+
+		for (int i = 0; i < _Size; i++)
+		{
+			if (OriginalArray[i] == Value)
+				return i;
+		}
+
+		return -1;
+
+	}
+
+	bool DeleteItem(T Value)
+	{
+		int index = Find(Value);
+
+		if (index == -1)
+			return false;
+
+		DeleteItemAt(index);
+		return true;
+	}
+
+	bool InsertAt(int index,T Value)
+	{
+		if (index > _Size || index < 0)
+			return false;
+
+		_Size++;
+
+		_TempArr = new T[_Size];
+
+		for (int i = 0; i < index; i++)
+			_TempArr[i] = OriginalArray[i];
+
+		_TempArr[index] = Value;
+
+		for (int i = index; i < _Size - 1; i++)
+			_TempArr[i + 1] = OriginalArray[i];
+
+		delete[] OriginalArray;
+		OriginalArray = _TempArr;
+		return true;
+
+	}
+
+	void InsertAtBeginning(T Value)
+	{
+		InsertAt(0,Value);
+	}
+
+	bool InsertAtEnd(T Value)
+	{
+		return InsertAt(_Size, Value);
+	}
+
+	bool InsertBefore(int index, T Value)
+	{
+		if (index < 1)
+			return InsertAt(0, Value);
+		else
+			return InsertAt(index - 1, Value);
+	}
+
+	bool InsertAfter(int index,T Value)
+	{
+		if (index >= _Size)
+			return InsertAt(_Size - 1, Value);
+		else
+			return InsertAt(index + 1, Value);
 	}
 
 };
