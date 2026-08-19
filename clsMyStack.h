583ab6d7678b937895dca0bf414c8c339923826d
Ahmed ALmehdar <ahmed.h.almehdar@gmail.com>
2026-08-19 03:43:52 +0300

    I Added new Satck Libaray and finish working in it

---
+++ clsMyStack.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include "clsMyQueue.h"
+
+
+using namespace std;
+
+template <class T>
+class clsMyStack : public clsMyQueue<T>
+{
+
+public:
+
+	void Push(T Value)
+	{
+		clsMyQueue<T>::_MyList.InsertAtBeginning(Value);
+	}
+
+	T Top()
+	{
+		return clsMyQueue<T>::front();
+	}
+
+	T Bottom()
+	{
+		return clsMyQueue<T>::back();
+	}
+
+};
